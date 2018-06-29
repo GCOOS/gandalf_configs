6b423c47efbc67669dfe38b13e316cf52485f75a
Bob Currier <robertdcurrier@gmail.com>
2018-06-29 14:33:23 -0400

    added style info to deployment.json

--- unit_541/unit_541-build_nc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/gncutils/scripts
-./dba_to_ngdac_profile_nc.py -p 1 \
-/data/gandalf/gandalf_configs/unit_541/ngdac \
-/data/gandalf/deployments/tamu/unit_541/processed_data/unit_541.merged.dba \
--o /data/gandalf/deployments/tamu/unit_541/ngdac_files
