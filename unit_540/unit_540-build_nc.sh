8062eef1a1a47a5a186b7fd0fd7644c672cc467c
Bob Currier <robertdcurrier@gmail.com>
2018-08-14 10:44:45 -0400

    added plot_url and kmz_url

--- unit_540/unit_540-build_nc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/gncutils/scripts
-./dba_to_ngdac_profile_nc.py -p 1 \
-/data/gandalf/gandalf_configs/unit_540/ngdac \
-/data/gandalf/deployments/tamu/unit_540/processed_data/unit_540.merged.dba \
--o /data/gandalf/deployments/tamu/unit_540/ngdac_files
